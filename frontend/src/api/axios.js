f60c9436b2ccb7e6ddfd6a59fef7a0d349c1d8c1
Sushpal <nenavathsushpal4@gmail.com>
2026-07-07 17:20:46 +0530

    changes

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios'
 
+
 const api = axios.create({
-    baseURL:         import.meta.env.VITE_API_URL || 'http://localhost:3000',
+    baseURL:import.meta.env.VITE_API_URL || 'http://localhost:3000',
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json'
@@ -18,3 +19,4 @@ api.interceptors.request.use((config) => {
 })
 
 export default api
+
